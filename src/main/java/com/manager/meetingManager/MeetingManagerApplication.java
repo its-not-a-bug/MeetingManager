84445f69c4f409e5b5fe2f3b5fff9ab8fdfdca4f
objectprogr <objectprogr@gmail.com>
2018-05-10 10:06:25 +0200

    init

---
+++ src/main/java/com/manager/meetingManager/MeetingManagerApplication.java
@@ -0,0 +1,12 @@
+package com.manager.meetingManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingManagerApplication.class, args);
+	}
+}
